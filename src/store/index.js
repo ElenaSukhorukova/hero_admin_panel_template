803e7efe61cd789c8580778fbb8fd45a56c24a87
Elena Sukhorukova <suhorukova.el@gmail.com>
2025-01-26 13:48:22 +0400

    change store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { thunk } from 'redux-thunk';
+// import { thunk } from 'redux-thunk';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
@@ -11,23 +11,24 @@ const stringMiddleware = () => next => action =>{
     return next(action);
 }
 
-const enhancer = store => next => action =>{
-    const oldDispatch = store.dispatch;
+// const enhancer = store => next => action =>{
+//     const oldDispatch = store.dispatch;
 
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return oldDispatch({
-                type: action
-            });
-        }
-    }
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') {
+//             return oldDispatch({
+//                 type: action
+//             });
+//         }
+//     }
 
-    return next(action);
-}
+//     return next(action);
+// }
 
 const store = configureStore({
     reducer: {heroes, filters},
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(enhancer).concat(thunk)
+    devTools: ProcessingInstruction.env.NODE_ENV !== 'production',
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(stringMiddleware) // .concat(enhancer) .concat(thunk)
 });
 
 export default store;
